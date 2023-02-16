76dc37dcef95d288257cf6db42ff07708f57e372
vjreddy <jayaramireddy.1619@gmail.com>
2023-02-16 20:14:47 +0000

    Spring-jenkins first commit

---
+++ src/main/java/com/jenkins/integration/SpringJenkinsApplication.java
@@ -0,0 +1,23 @@
+package com.jenkins.integration;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+	public static Logger logger=LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+	@PostConstruct
+	public void init() {
+		logger.info("Application started..");
+	}
+	public static void main(String[] args) {
+		logger.info("Application executed..");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
